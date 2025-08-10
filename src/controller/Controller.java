81a0e9debb86b5e3e51da8de02df032f37c83b96
Hadi Yaghi <hadiyaghi.ceng.2006@gmail.com>
2025-08-10 14:51:52 +0300

    Update Controller.java

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet; // import for result sets ((to get the result of the 
 import java.sql.SQLException; // import for SQL exceptions
 import java.sql.Time; // import for SQL Time
 import java.util.ArrayList; // import for ArrayList
-import java.util.List; // import for List
+import java.util.List; 
 import java.util.Random; // import for Random ID generation
 import java.util.logging.Level; // import for logging levels((Both of these metohd is used to clarifie the error in details EX:Date,time))
 import java.util.logging.Logger; // import for logging      ((Both of these metohd is used to clarifie the error in details EX:Date,time))
